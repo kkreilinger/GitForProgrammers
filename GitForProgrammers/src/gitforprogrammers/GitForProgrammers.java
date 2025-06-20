187ffcae103d6e0914f74a9dd6aa2e5bb7e642de
Kurt Kreilinger <k.kreilinger@gmail.com>
2025-06-20 20:45:44 +0200

    Add informative Lines

--- GitForProgrammers/src/gitforprogrammers/GitForProgrammers.java
+++ GitForProgrammers/src/gitforprogrammers/GitForProgrammers.java
@@ -5,5 +5,6 @@ public class GitForProgrammers {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("I added this in Visual Studio Code");
     } 
 }
