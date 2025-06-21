68d7d92f1d8a542275cbf0bfdfb899a5f95ab369
Kurt Kreilinger <k.kreilinger@gmail.com>
2025-06-21 10:14:06 +0200

    Add println indicating we are in command line

--- GitForProgrammers/src/gitforprogrammers/GitForProgrammers.java
+++ GitForProgrammers/src/gitforprogrammers/GitForProgrammers.java
@@ -6,5 +6,6 @@ public class GitForProgrammers {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("I added this in Visual Studio Code");
+        System.out.println("I added this in the command line repository");
     } 
 }
