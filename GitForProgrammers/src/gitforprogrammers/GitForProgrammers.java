97c7ec28279201380b83c7e46dd914cb137aa999
Kurt Kreilinger <k.kreilinger@gmail.com>
2025-06-21 15:26:43 +0200

    my message

--- GitForProgrammers/src/gitforprogrammers/GitForProgrammers.java
+++ GitForProgrammers/src/gitforprogrammers/GitForProgrammers.java
@@ -7,5 +7,6 @@ public class GitForProgrammers {
         System.out.println("Hello world!");
         System.out.println("I added this in Visual Studio Code");
         System.out.println("I added this in the command line repository");
+        System.out.println("This line added to show the staging area");
     } 
 }
