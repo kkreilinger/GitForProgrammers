df40109a8030e004da43439eac94dabc4fc674d0
Kurt Kreilinger <k.kreilinger@gmail.com>
2025-06-22 16:32:16 +0200

    Add substract method

--- GitForProgrammers/src/gitforprogrammers/Calculator.java
+++ GitForProgrammers/src/gitforprogrammers/Calculator.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package gitforprogrammers;
 
-/**
- *
- * @author kkrei
- */
 public class Calculator {
     
+    public int add(int left, int right) {
+        return left + right;
+    }
+    
+    public int subtract(int left, int right) {
+        return left - right;
+    }
 }
