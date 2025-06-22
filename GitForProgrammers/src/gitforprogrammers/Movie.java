c494420a40ab1ea8760d791c58477781ee381999
Kurt Kreilinger <k.kreilinger@gmail.com>
2025-06-22 09:49:09 +0200

    Update Movie.java

--- GitForProgrammers/src/gitforprogrammers/Movie.java
+++ GitForProgrammers/src/gitforprogrammers/Movie.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package gitforprogrammers;
 
-/**
- *
- * @author kkrei
- */
+import java.util.ArrayList;
+
 public class Movie {
-    
+    String title;
+    ArrayList<String> actors = new ArrayList<>();
 }
